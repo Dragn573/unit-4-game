41af49dcb8278217471677098d2be65d4f4454be
Calvin <Calvin.d.Bowman@gmail.com>
2018-05-26 17:51:53 -0400

    up3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,7 @@
-<script type="text/javascript" src="script.js"></script>
 var totalScore = 120;
 $("#score-to-click").text(totalScore);
 var crystals = $("crystals");
 var counter = 0;
 var numberOptions = [15,10,4,1];
-for (var i = 0; i < numberOptions.length; i++) {
-var imageCrystal = $("<img>")};
-imageCrystal.addClass("crystal-image");
-
-imageCrystal.attr("src= assets/images/blue crystal.jpg",)
-
-crystals.append(imageCrystal);
-
+for (var i = 0; i < numberOptions.length; i++) 
 
