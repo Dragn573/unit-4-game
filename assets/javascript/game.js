1c14f1500d6e651c8f1c7fb753098fb1737593c5
Calvin <Calvin.d.Bowman@gmail.com>
2018-05-26 22:13:15 -0400

    trying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
-var totalScore = 120;
-$("#score-to-click").text(totalScore);
-var crystals = $("crystals");
+var Random=Math.floor(Math.Random()*101+19)
+$("#score-to-click").on("click", "crystal", function() {
+var crystal = $("crystal Image");
 var counter = 0;
 var numberOptions = [15,10,4,1];
-for (var i = 0; i < numberOptions.length; i++) 
 
+
+for (var i = 0; i < numberOptions.length; i++)
